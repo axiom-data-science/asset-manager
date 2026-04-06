2512c97c2d44727dc2dbb24a01aeebb3948eebdf
Brian Stone <brian@axds.co>
2026-04-05 22:04:29 -0800

    fix object type display

--- src/manage/document/list.tsx
+++ src/manage/document/list.tsx
@@ -1,11 +1,14 @@
 import { useDocumentList } from "@/manage/document/useDocumentList"
 import { SelectInput, Table, ViewWithLoader } from "@axdspub/axiom-ui-utilities"
 import type { ReactElement } from "react"
+import { useObjectTypeList } from "../object_type/useObjectTypeList"
+import type { IObjectType } from "@/types/types"
 
 
-const ListDocuments = (): ReactElement => {
+const ListDocuments = ({ object_types }: { object_types: IObjectType[] }): ReactElement => {
 
     const { data: documents, isLoading, error } = useDocumentList({}, [***REMOVED***owner_sub***REMOVED***, ***REMOVED***object_type_uuid***REMOVED***])
+    const object_types_map = Object.fromEntries(object_types.map(ot => [ot.uuid, ot]))
 
     return (
         <ViewWithLoader isLoading={isLoading} error={error} data={documents}>
@@ -18,7 +21,7 @@ const ListDocuments = (): ReactElement => {
                         if (rollup?.length === 0) return null;
                         return (
                             <div className=***REMOVED***flex flex-row gap-2***REMOVED*** key={r}>
-                                <span className=***REMOVED***font-semibold***REMOVED***>{r.split(***REMOVED***_***REMOVED***).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(***REMOVED*** ***REMOVED***)}</span>
+                                <span className=***REMOVED***font-semibold***REMOVED***>{r.split(***REMOVED***_***REMOVED***).filter((w,i) => i < 1 || w.toLowerCase() !== ***REMOVED***uuid***REMOVED***).map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(***REMOVED*** ***REMOVED***)}</span>
                                 <SelectInput
                                     id={r}
                                     testId={r}
@@ -26,7 +29,7 @@ const ListDocuments = (): ReactElement => {
                                     label={null}
                                     size=***REMOVED***xs***REMOVED***
                                     options={rollup?.map(item => ({
-                                        label: `${item.label} (${item.count})`,
+                                        label: `${r === ***REMOVED***object_type_uuid***REMOVED*** ? object_types_map[item.label]?.label : item.label} (${item.count})`,
                                         value: item.label
                                     })) ?? []}
                                 />
@@ -49,7 +52,8 @@ const ListDocuments = (): ReactElement => {
                             },
                             {
                                 label: ***REMOVED***Type***REMOVED***,
-                                id: ***REMOVED***type***REMOVED***
+                                id: ***REMOVED***object_type_uuid***REMOVED***,
+                                accessor: r => object_types_map[r.object_type_uuid]?.label ?? r.object_type_uuid
                             },
                             {
                                 label: ***REMOVED***Owner***REMOVED***,
@@ -66,4 +70,15 @@ const ListDocuments = (): ReactElement => {
 
 }
 
-export default ListDocuments
+const ListDocumentsLoader = (): ReactElement => {
+    const {data: object_types, isLoading, error} = useObjectTypeList()
+    return (
+        <ViewWithLoader isLoading={isLoading} error={error} data={object_types}>
+            {
+                object_types && <ListDocuments object_types={object_types.items} />
+            }
+        </ViewWithLoader>
+    )
+}
+
+export default ListDocumentsLoader

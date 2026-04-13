282914892346a148ed356e76120b856d4ebeda63
Brian Stone <brian@axds.co>
2026-04-12 22:42:56 -0800

    use default form to edit document rather than schema form

--- src/manage/document/edit.tsx
+++ src/manage/document/edit.tsx
@@ -1,143 +1,197 @@
-import { useAuth } from "@/auth/useAuth";
-import { patchDocument } from "@/manage/document/services";
-import { type IValidationError, type IObjectSchema } from "@/types/types";
-import type { IDocument } from "@/types/types";
-import { FormCreator, schemaToFormUtils, type IForm, type IFormValues } from "@axdspub/axiom-ui-forms";
-import { Button, Loader, ViewWithLoader } from "@axdspub/axiom-ui-utilities";
-import { useState, type ReactElement } from "react";
-import { useNavigate, useParams } from "react-router-dom"
-import { useDefaultObjectSchemaAtUUID } from "@/manage/object_schema/useDefaultSchemaForType";
-import { omit } from "lodash-es";
-import { validate } from "@/lib/utils";
-import Errors from "@/manage/components/errors";
-import { useDocument } from "./useDocument";
+import { useAuth } from ***REMOVED***@/auth/useAuth***REMOVED***
+import { patchDocument } from ***REMOVED***@/manage/document/services***REMOVED***
+import { type IValidationError, type IObjectSchema } from ***REMOVED***@/types/types***REMOVED***
+import type { IAssetForm, IDocument, IFormToFieldConfigWithDetails } from ***REMOVED***@/types/types***REMOVED***
+import {
+  FormCreator,
+  schemaToFormUtils,
+  type IForm,
+  type IFormFieldOverride,
+  type IFormOverride,
+  type IFormValues,
+} from ***REMOVED***@axdspub/axiom-ui-forms***REMOVED***
+import { Button, Loader, ViewWithLoader } from ***REMOVED***@axdspub/axiom-ui-utilities***REMOVED***
+import { useState, type ReactElement } from ***REMOVED***react***REMOVED***
+import { useNavigate, useParams } from ***REMOVED***react-router-dom***REMOVED***
+import { omit } from ***REMOVED***lodash-es***REMOVED***
+import { validate } from ***REMOVED***@/lib/utils***REMOVED***
+import Errors from ***REMOVED***@/manage/components/errors***REMOVED***
+import { documentQueryKey, useDocument } from ***REMOVED***./useDocument***REMOVED***
+import { useFullDefaultFormAtObjectType } from ***REMOVED***../form/useForm***REMOVED***
+import { useQueryClient } from ***REMOVED***@tanstack/react-query***REMOVED***
 
 const EditDocumentForm = ({
-    document,
-    schema
+  document,
+  schema,
+  assetForm,
+  fieldConfigs,
 }: {
-    document: IDocument,
-    schema: IObjectSchema
+  document: IDocument
+  schema: IObjectSchema
+  assetForm?: IAssetForm | null
+  fieldConfigs?: IFormToFieldConfigWithDetails[]
 }): ReactElement => {
-    const navigate = useNavigate()
-    const [saving, setSaving] = useState(false);
-    const [formValues, setFormValues] = useState<IFormValues>(document.data as unknown as IFormValues);
-    const [errors, setErrors] = useState<IValidationError[]>([])
-    const auth = useAuth()
-    const defaultForm: IForm = {
-        id: ***REMOVED***create-document***REMOVED***,
-        settings: {
-            show_progress: false
+  const navigate = useNavigate()
+  const [saving, setSaving] = useState(false)
+  const [formValues, setFormValues] = useState<IFormValues>(document.data as unknown as IFormValues)
+  const [errors, setErrors] = useState<IValidationError[]>([])
+  const auth = useAuth()
+  const queryClient = useQueryClient()
+  const defaultForm: IForm = {
+    id: ***REMOVED***create-document***REMOVED***,
+    settings: {
+      show_progress: false,
+    },
+    fields: [
+      {
+        id: ***REMOVED***label***REMOVED***,
+        label: ***REMOVED***Label***REMOVED***,
+        type: ***REMOVED***text***REMOVED***,
+        required: true,
+      },
+      {
+        id: ***REMOVED***description***REMOVED***,
+        label: ***REMOVED***Description***REMOVED***,
+        type: ***REMOVED***long_text***REMOVED***,
+        required: true,
+      },
+      {
+        id: ***REMOVED***data***REMOVED***,
+        label: ***REMOVED***Data***REMOVED***,
+        type: ***REMOVED***json***REMOVED***,
+      },
+    ],
+  }
+  //const dataForm = omit(schemaToFormUtils.schemaToFormObject(schema.json_schema), ***REMOVED***label***REMOVED***)
+  const fieldConfigJSON =
+    fieldConfigs?.map((fc) => fc.fields_override_config.config as unknown as IFormFieldOverride) ??
+    []
+  const dataForm = (
+    assetForm?.use_form_config === true &&
+    assetForm?.form_config !== undefined &&
+    assetForm?.form_config !== null
+      ? assetForm.form_config
+      : assetForm?.schema_override_config !== undefined || fieldConfigJSON !== undefined
+        ? omit(
+            schemaToFormUtils.overridesAndSchemaToFormObject({
+              schema: schema.json_schema,
+              formOverrides: assetForm?.schema_override_config
+                ? [assetForm?.schema_override_config as IFormOverride]
+                : undefined,
+              formFieldOverrides: fieldConfigJSON ? [fieldConfigJSON] : undefined,
+            }),
+            ***REMOVED***label***REMOVED***
+          )
+        : schemaToFormUtils.schemaToFormObject(schema.json_schema)
+  ) as IForm
+  const form =
+    dataForm.fields?.length ||
+    dataForm.pages?.length ||
+    dataForm.wizard_steps?.length ||
+    dataForm.tabs?.length
+      ? dataForm
+      : defaultForm
+
+  const onSave = async () => {
+    setSaving(true)
+    const valid = await validate({ form, formValues })
+    if (!valid.valid && valid.errors.length > 0) {
+      setErrors(valid.errors)
+      setSaving(false)
+      window.scrollTo({
+        top: 0,
+        behavior: ***REMOVED***smooth***REMOVED***, // Adds a gradual animation
+      })
+      queryClient.invalidateQueries({
+        queryKey: documentQueryKey(document.uuid),
+      })
+      return
+    }
+    setErrors([])
+    try {
+      await patchDocument({
+        uuid: document.uuid,
+        document: {
+          uuid: document.uuid,
+          label: formValues.label ?? formValues.title ?? ***REMOVED***Untitled Document***REMOVED***,
+          description: formValues.description ?? ***REMOVED******REMOVED***,
+          data: formValues,
+        } as Omit<IDocument, ***REMOVED***created_at***REMOVED*** | ***REMOVED***updated_at***REMOVED***>,
+        token: auth.user?.access_token ?? ***REMOVED******REMOVED***,
+      })
+
+      setSaving(false)
+      navigate(***REMOVED***/document***REMOVED***)
+    } catch (e: unknown) {
+      setSaving(false)
+      setErrors([
+        {
+          field: ***REMOVED***form***REMOVED***,
+          message: (e as Error)?.message ?? ***REMOVED***An error occurred while saving. Please try again.***REMOVED***,
         },
-        fields: [
-            {
-                id: ***REMOVED***label***REMOVED***,
-                label: ***REMOVED***Label***REMOVED***,
-                type: ***REMOVED***text***REMOVED***,
-                required: true
-            },
-            {
-                id: ***REMOVED***description***REMOVED***,
-                label: ***REMOVED***Description***REMOVED***,
-                type: ***REMOVED***long_text***REMOVED***,
-                required: true
-            },
-            {
-                id: ***REMOVED***data***REMOVED***,
-                label: ***REMOVED***Data***REMOVED***,
-                type: ***REMOVED***json***REMOVED***
-            }
-
-        ]
+      ])
+      window.scrollTo({
+        top: 0,
+        behavior: ***REMOVED***smooth***REMOVED***, // Adds a gradual animation
+      })
     }
-    const dataForm = omit(schemaToFormUtils.schemaToFormObject(schema.json_schema), ***REMOVED***label***REMOVED***)
-    const form = dataForm.fields?.length || dataForm.pages?.length || dataForm.wizard_steps?.length || dataForm.tabs?.length ? dataForm : defaultForm
+  }
 
-    const onSave = async () => {
-        setSaving(true);
-        const valid = await validate({ form, formValues })
-        if (!valid.valid && valid.errors.length > 0) {
-            setErrors(valid.errors);
-            setSaving(false);
-            window.scrollTo({
-                top: 0,
-                behavior: ***REMOVED***smooth***REMOVED*** // Adds a gradual animation
-            })
-            return;
-        }
-        setErrors([])
-        try {
-            await patchDocument({
-                uuid: document.uuid,
-                document: {
-                    uuid: document.uuid,
-                    label: formValues.label ?? formValues.title ?? ***REMOVED***Untitled Document***REMOVED***,
-                    description: formValues.description ?? ***REMOVED******REMOVED***,
-                    data: formValues
-                } as Omit<IDocument, ***REMOVED***created_at***REMOVED*** | ***REMOVED***updated_at***REMOVED***>,
-                token: auth.user?.access_token ?? ***REMOVED******REMOVED***
-            })
-
-            setSaving(false);
-            navigate(***REMOVED***/document***REMOVED***)
-
-        } catch (e: unknown) {
-            setSaving(false);
-            setErrors([{ field: ***REMOVED***form***REMOVED***, message: (e as Error)?.message ?? ***REMOVED***An error occurred while saving. Please try again.***REMOVED*** }])
-            window.scrollTo({
-                top: 0,
-                behavior: ***REMOVED***smooth***REMOVED*** // Adds a gradual animation
-            })
-        }
-
-    }
-
-
-    return (
-        <div className=***REMOVED***flex flex-col gap-4 relative***REMOVED***>
-            <h1 className=***REMOVED***text-2xl font-bold***REMOVED***>Edit document</h1>
-            <Errors errors={errors} />
-            <FormCreator form={form} formValueState={[formValues, setFormValues]} />
-            <div className=***REMOVED***flex flex-row gap-4  p-4 sticky bottom-0 bg-white/80 z-10***REMOVED***>
-                <Button onClick={onSave} type=***REMOVED***primary***REMOVED*** disabled={saving}>{saving ? <Loader className="animate-spin" /> : ***REMOVED***Save***REMOVED***}</Button>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex flex-col gap-4 relative">
+      <h1 className="text-2xl font-bold">Edit document</h1>
+      <Errors errors={errors} />
+      <FormCreator
+        form={{
+          ...form,
+          label: undefined,
+          settings: {
+            ...form.settings,
+            url_navigable: false,
+          },
+        }}
+        formValueState={[formValues, setFormValues]}
+      />
+      <div className="flex flex-row gap-4  p-4 sticky bottom-0 bg-white/80 z-10">
+        <Button onClick={onSave} type="primary" disabled={saving}>
+          {saving ? <Loader className="animate-spin" /> : ***REMOVED***Save***REMOVED***}
+        </Button>
+      </div>
+    </div>
+  )
 }
-
-
-
 
 const LoadSchemaAndCreateDocumentForm = ({ document }: { document: IDocument }): ReactElement => {
-    const { data: object_schema, isLoading, error } = useDefaultObjectSchemaAtUUID(document.object_type_uuid)
-    return <ViewWithLoader isLoading={isLoading} error={error} data={object_schema}>
-        {
-            object_schema && <EditDocumentForm document={document} schema={object_schema} />
-        }
+  const { data, isLoading, error } = useFullDefaultFormAtObjectType({
+    object_type_uuid: document.object_type_uuid,
+  })
+  return (
+    <ViewWithLoader isLoading={isLoading} error={error} data={data}>
+      {data && (
+        <EditDocumentForm document={document} schema={data.object_schema} assetForm={data.form} />
+      )}
     </ViewWithLoader>
+  )
 }
-
 
 const EditDocument = (): ReactElement => {
-    const uuid = useParams().uuid ?? null
+  const uuid = useParams().uuid ?? null
 
-    const { data: document, isLoading, error } = useDocument(uuid)
-    if (uuid === null || uuid === undefined) {
-        return (
-            <div className="p-20">
-                <p>No document specified. Please select a document to edit.</p>
-            </div>
-        )
-    }
-    return <ViewWithLoader isLoading={isLoading} error={error} data={document}>
-        {
-            document && <LoadSchemaAndCreateDocumentForm document={document as IDocument} />
-        }
+  const { data: document, isLoading, error } = useDocument(uuid)
+  if (uuid === null || uuid === undefined) {
+    return (
+      <div className="p-20">
+        <p>No document specified. Please select a document to edit.</p>
+      </div>
+    )
+  }
+  return (
+    <ViewWithLoader isLoading={isLoading} error={error} data={document}>
+      {document && <LoadSchemaAndCreateDocumentForm document={document as IDocument} />}
     </ViewWithLoader>
+  )
 
-    //return <ObjectTypeLoader urlRoot="/document/create" View={LoadSchemaAndCreateDocumentForm} />
+  //return <ObjectTypeLoader urlRoot="/document/create" View={LoadSchemaAndCreateDocumentForm} />
 }
-
-
 
 export default EditDocument
--- src/manage/form/useForm.ts
+++ src/manage/form/useForm.ts
@@ -1,135 +1,200 @@
-import { useAuth } from "@/auth/useAuth"
-import { queryOptions, useQuery } from "@tanstack/react-query"
-import { fetchDefaultFormAtObjectType, fetchForm } from "./services"
-import { useCombinedQueries } from "@/hooks/use-combined-queries"
-import { getObjectTypeListQuery } from "@/manage/object_type/useObjectTypeList"
-import { getFieldConfigListAtFormQuery } from "../field_config/useFieldConfigList"
-import { fetchSchemaAtForm } from "@/manage/object_schema/services"
-import { postgrestArgs } from "@/services/postgrest/endpoints"
-import type { IPostgrestParams } from "@/types/types"
-import { getObjectSchemaAndObjectTypeQuery } from "../object_schema/useObjectSchema"
+import { useAuth } from ***REMOVED***@/auth/useAuth***REMOVED***
+import { queryOptions, useQuery } from ***REMOVED***@tanstack/react-query***REMOVED***
+import { fetchDefaultFormAtObjectType, fetchForm } from ***REMOVED***./services***REMOVED***
+import { useCombinedQueries } from ***REMOVED***@/hooks/use-combined-queries***REMOVED***
+import { getObjectTypeListQuery } from ***REMOVED***@/manage/object_type/useObjectTypeList***REMOVED***
+import { getFieldConfigListAtFormQuery } from ***REMOVED***../field_config/useFieldConfigList***REMOVED***
+import { fetchSchemaAtForm } from ***REMOVED***@/manage/object_schema/services***REMOVED***
+import { postgrestArgs } from ***REMOVED***@/services/postgrest/endpoints***REMOVED***
+import type { IPostgrestParams } from ***REMOVED***@/types/types***REMOVED***
+import { getObjectSchemaAndObjectTypeQuery } from ***REMOVED***../object_schema/useObjectSchema***REMOVED***
 
-export const formQueryKey = (uuid?: string, params?: IPostgrestParams) => [***REMOVED***form***REMOVED***, uuid, postgrestArgs(params ?? {}).toString()]
+export const formQueryKey = (uuid?: string, params?: IPostgrestParams) => [
+  ***REMOVED***form***REMOVED***,
+  uuid,
+  postgrestArgs(params ?? {}).toString(),
+]
 export const formWithLookupsQueryKey = (uuid?: string) => [***REMOVED***form***REMOVED***, ***REMOVED***object-types***REMOVED***, uuid]
-export const formAtObjectTypeQueryKey = ({object_type_uuid}: {object_type_uuid?: string}) => [***REMOVED***form***REMOVED***, ***REMOVED***object_type***REMOVED***, object_type_uuid]
-export const fullFormAtObjectTypeQueryKey = ({object_type_uuid}: {object_type_uuid?: string}) => [***REMOVED***form***REMOVED***, ***REMOVED***object_type***REMOVED***, object_type_uuid, ***REMOVED***full***REMOVED***]
+export const formAtObjectTypeQueryKey = ({
+  object_type_uuid,
+  params,
+}: {
+  object_type_uuid?: string
+  params?: IPostgrestParams
+}) => [***REMOVED***form***REMOVED***, ***REMOVED***object_type***REMOVED***, object_type_uuid, postgrestArgs(params ?? {}).toString()]
+export const fullFormAtObjectTypeQueryKey = ({
+  object_type_uuid,
+}: {
+  object_type_uuid?: string
+}) => [***REMOVED***form***REMOVED***, ***REMOVED***object_type***REMOVED***, object_type_uuid, ***REMOVED***full***REMOVED***]
 
-export const getFormQuery = ({uuid, token, params}: {uuid?: string, token?: string, params?: IPostgrestParams}) => {
-    return queryOptions({
-        queryKey: formQueryKey(uuid,params),
-        enabled: !!uuid && !!token && uuid !== ***REMOVED******REMOVED***,
-        queryFn: async ({ signal }) => {
-            
-            const objectSchema = await fetchForm({
-                uuid: uuid ?? ***REMOVED***NA***REMOVED***,
-                signal,
-                token: token ?? ***REMOVED******REMOVED***,
-                params
-            })
+export const getFormQuery = ({
+  uuid,
+  token,
+  params,
+}: {
+  uuid?: string
+  token?: string
+  params?: IPostgrestParams
+}) => {
+  return queryOptions({
+    queryKey: formQueryKey(uuid, params),
+    enabled: !!uuid && !!token && uuid !== ***REMOVED******REMOVED***,
+    queryFn: async ({ signal }) => {
+      const objectSchema = await fetchForm({
+        uuid: uuid ?? ***REMOVED***NA***REMOVED***,
+        signal,
+        token: token ?? ***REMOVED******REMOVED***,
+        params,
+      })
 
-            return objectSchema
-            
-        }
-    })
+      return objectSchema
+    },
+  })
 }
 
-export const getFormAtObjectTypeQuery = ({object_type_uuid, params, token}: {object_type_uuid?: string, params?: IPostgrestParams, token?: string}) => {
-    return queryOptions({
-        queryKey: formAtObjectTypeQueryKey({object_type_uuid}),
-        enabled: !!object_type_uuid && !!token && object_type_uuid !== ***REMOVED******REMOVED***,
-        queryFn: async ({ signal }) => {
-            const form = await fetchDefaultFormAtObjectType({
-                object_type_uuid: object_type_uuid ?? ***REMOVED***NA***REMOVED***,
-                params,
-                signal,
-                token: token ?? ***REMOVED******REMOVED***
-            })
+export const getFormAtObjectTypeQuery = ({
+  object_type_uuid,
+  params,
+  token,
+}: {
+  object_type_uuid?: string
+  params?: IPostgrestParams
+  token?: string
+}) => {
+  return queryOptions({
+    queryKey: formAtObjectTypeQueryKey({ object_type_uuid, params }),
+    enabled: !!object_type_uuid && !!token && object_type_uuid !== ***REMOVED******REMOVED***,
+    queryFn: async ({ signal }) => {
+      const form = await fetchDefaultFormAtObjectType({
+        object_type_uuid: object_type_uuid ?? ***REMOVED***NA***REMOVED***,
+        params,
+        signal,
+        token: token ?? ***REMOVED******REMOVED***,
+      })
 
-            return form
-        }
-    })
-
+      return form
+    },
+  })
 }
 
-export const getSchemaAtFormQuery = ({form_uuid, params, token}: {form_uuid?: string, params?: IPostgrestParams, token?: string}) => {
-    return queryOptions({
-        queryKey: [***REMOVED***form***REMOVED***, ***REMOVED***object_schema***REMOVED***, form_uuid, postgrestArgs(params ?? {}).toString()],
-        enabled: !!form_uuid && !!token && form_uuid !== ***REMOVED******REMOVED***,
-        queryFn: async ({ signal }) => {
-            const objectSchema = await fetchSchemaAtForm({
-                form_uuid: form_uuid ?? ***REMOVED***NA***REMOVED***,
-                params,
-                signal,
-                token: token ?? ***REMOVED******REMOVED***
-            })
-            return objectSchema
-        }
-    })
+export const getSchemaAtFormQuery = ({
+  form_uuid,
+  params,
+  token,
+}: {
+  form_uuid?: string
+  params?: IPostgrestParams
+  token?: string
+}) => {
+  return queryOptions({
+    queryKey: [***REMOVED***form***REMOVED***, ***REMOVED***object_schema***REMOVED***, form_uuid, postgrestArgs(params ?? {}).toString()],
+    enabled: !!form_uuid && !!token && form_uuid !== ***REMOVED******REMOVED***,
+    queryFn: async ({ signal }) => {
+      const objectSchema = await fetchSchemaAtForm({
+        form_uuid: form_uuid ?? ***REMOVED***NA***REMOVED***,
+        params,
+        signal,
+        token: token ?? ***REMOVED******REMOVED***,
+      })
+      return objectSchema
+    },
+  })
 }
 
 export const useForm = (uuid?: string) => {
-    const auth = useAuth()
-    const queryResult = useQuery(getFormQuery({ uuid, token: auth.user?.access_token }))
-    return queryResult
+  const auth = useAuth()
+  const queryResult = useQuery(getFormQuery({ uuid, token: auth.user?.access_token }))
+  return queryResult
 }
 
-export const useFullForm = ({form_uuid}: {form_uuid?: string}) => {
-    const auth = useAuth()
-    const form = useQuery(getFormQuery({uuid: form_uuid, token: auth.user?.access_token, params: {
-        select: [***REMOVED***object_type_uuid***REMOVED***]
-    }}))
-    const queryObjects = {
-        form: getFormQuery({uuid: form_uuid, token: auth.user?.access_token}),
-        field_configs: getFieldConfigListAtFormQuery({form_uuid: form_uuid ?? ***REMOVED***NA***REMOVED***, token: auth.user?.access_token ?? ***REMOVED******REMOVED***}),
-        object_schema: {
-            ...getObjectSchemaAndObjectTypeQuery({object_type_uuid: form.data?.object_type_uuid ?? ***REMOVED***NA***REMOVED***, token: auth.user?.access_token ?? ***REMOVED******REMOVED***}),
-            enabled: !!form.data && !!auth.user?.access_token && form.data.object_type_uuid !== ***REMOVED******REMOVED***
-        }
-    }
-    return useCombinedQueries(queryObjects)
+export const useFullForm = ({ form_uuid }: { form_uuid?: string }) => {
+  const auth = useAuth()
+  const form = useQuery(
+    getFormQuery({
+      uuid: form_uuid,
+      token: auth.user?.access_token,
+      params: {
+        select: [***REMOVED***object_type_uuid***REMOVED***],
+      },
+    })
+  )
+  const queryObjects = {
+    form: getFormQuery({ uuid: form_uuid, token: auth.user?.access_token }),
+    field_configs: getFieldConfigListAtFormQuery({
+      form_uuid: form_uuid ?? ***REMOVED***NA***REMOVED***,
+      token: auth.user?.access_token ?? ***REMOVED******REMOVED***,
+    }),
+    object_schema: {
+      ...getObjectSchemaAndObjectTypeQuery({
+        object_type_uuid: form.data?.object_type_uuid ?? ***REMOVED***NA***REMOVED***,
+        token: auth.user?.access_token ?? ***REMOVED******REMOVED***,
+      }),
+      enabled: !!form.data && !!auth.user?.access_token && form.data.object_type_uuid !== ***REMOVED******REMOVED***,
+    },
+  }
+  return useCombinedQueries(queryObjects)
 }
 
+export const useFormWithLookups = ({ uuid }: { uuid?: string }) => {
+  const auth = useAuth()
+  const queryObjects = {
+    form: getFormQuery({ uuid, token: auth.user?.access_token }),
+    object_types: getObjectTypeListQuery({ token: auth.user?.access_token }),
+    field_configs: getFieldConfigListAtFormQuery({
+      form_uuid: uuid ?? ***REMOVED***NA***REMOVED***,
+      token: auth.user?.access_token ?? ***REMOVED******REMOVED***,
+    }),
+  }
 
-export const  useFormWithLookups = ({uuid}: {uuid?: string}) => {
-    const auth = useAuth()
-    const queryObjects = {
-        form: getFormQuery({uuid, token: auth.user?.access_token}),
-        object_types: getObjectTypeListQuery({token: auth.user?.access_token}),
-        field_configs: getFieldConfigListAtFormQuery({form_uuid: uuid ?? ***REMOVED***NA***REMOVED***, token: auth.user?.access_token ?? ***REMOVED******REMOVED***})
-    }   
-
-    return useCombinedQueries(queryObjects)
+  return useCombinedQueries(queryObjects)
 }
 
 export const useDefaultFormAtObjectType = ({ object_type_uuid }: { object_type_uuid?: string }) => {
-    const auth = useAuth()
-    const queryResult = useQuery(getFormAtObjectTypeQuery({ object_type_uuid, token: auth.user?.access_token }))
-    return queryResult
+  const auth = useAuth()
+  const queryResult = useQuery(
+    getFormAtObjectTypeQuery({ object_type_uuid, token: auth.user?.access_token })
+  )
+  return queryResult
 }
 
+export const useFullDefaultFormAtObjectType = ({
+  object_type_uuid,
+}: {
+  object_type_uuid?: string
+}) => {
+  const auth = useAuth()
+  const form = useQuery(
+    getFormAtObjectTypeQuery({
+      object_type_uuid,
+      token: auth.user?.access_token,
+      params: {
+        select: [***REMOVED***uuid***REMOVED***],
+      },
+    })
+  )
+  const queryObjects = {
+    //form: getFormAtObjectTypeQuery({object_type_uuid, token: auth.user?.access_token}),
+    form: getFormAtObjectTypeQuery({ object_type_uuid, token: auth.user?.access_token }),
+    object_types: {
+      ...getObjectTypeListQuery({ token: auth.user?.access_token }),
+      enabled: !!form.data && !!auth.user?.access_token && object_type_uuid !== ***REMOVED******REMOVED***,
+    },
+    field_configs: {
+      ...getFieldConfigListAtFormQuery({
+        form_uuid: form?.data?.uuid ?? ***REMOVED***NA***REMOVED***,
+        token: auth.user?.access_token ?? ***REMOVED******REMOVED***,
+      }),
+      enabled: !!form.data && !!auth.user?.access_token && object_type_uuid !== ***REMOVED******REMOVED***,
+    },
+    object_schema: {
+      ...getSchemaAtFormQuery({
+        form_uuid: form?.data?.uuid ?? ***REMOVED***NA***REMOVED***,
+        token: auth.user?.access_token ?? ***REMOVED******REMOVED***,
+      }),
+      enabled: !!form.data && !!auth.user?.access_token && object_type_uuid !== ***REMOVED******REMOVED***,
+    },
+  }
 
-
-export const useFullDefaultFormAtObjectType = ({ object_type_uuid }: { object_type_uuid?: string }) => {
-    const auth = useAuth()
-    const form = useQuery(getFormAtObjectTypeQuery({object_type_uuid, token: auth.user?.access_token, params: {
-        select: [***REMOVED***uuid***REMOVED***]
-    }}))
-    const queryObjects = {
-        //form: getFormAtObjectTypeQuery({object_type_uuid, token: auth.user?.access_token}),
-        form: getFormAtObjectTypeQuery({object_type_uuid, token: auth.user?.access_token}),
-        object_types: {
-            ...getObjectTypeListQuery({token: auth.user?.access_token}),
-            enabled: !!form.data && !!auth.user?.access_token && object_type_uuid !== ***REMOVED******REMOVED***
-        },
-        field_configs: {
-            ...getFieldConfigListAtFormQuery({form_uuid: form?.data?.uuid ?? ***REMOVED***NA***REMOVED***, token: auth.user?.access_token ?? ***REMOVED******REMOVED***}),
-            enabled: !!form.data && !!auth.user?.access_token && object_type_uuid !== ***REMOVED******REMOVED***
-        },
-        object_schema: {
-            ...getSchemaAtFormQuery({form_uuid: form?.data?.uuid ?? ***REMOVED***NA***REMOVED***, token: auth.user?.access_token ?? ***REMOVED******REMOVED***}),
-            enabled: !!form.data && !!auth.user?.access_token && object_type_uuid !== ***REMOVED******REMOVED***
-        }
-    }
-    
-    return useCombinedQueries(queryObjects)
+  return useCombinedQueries(queryObjects)
 }

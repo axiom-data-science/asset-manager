13c9b203a0d6a4661161d9822f5d2922e2f74f0f
Brian Stone <brian@axds.co>
2026-07-17 13:22:45 -0800

    pass default props object if none

--- src/BrandComponents.tsx
+++ src/BrandComponents.tsx
@@ -27,7 +27,7 @@ export const getBrandComponent = (brand: string | undefined, component: keyof IB
     const brandKey = brand as keyof typeof BrandComponents
     if (brand && BrandComponents[brandKey] && BrandComponents[brandKey][component]) {
         return component === ***REMOVED***CreateDocumentEntry***REMOVED***
-            ? BrandComponents[brandKey][component](props!)
+            ? BrandComponents[brandKey][component](props ?? {})
             : BrandComponents[brandKey][component]()
     }
     return null
